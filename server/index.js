7e22e23b8fa34ce3d27083f65ee2c3afd1be4a9e
Darshana <darshana.kadam2003@gmail.com>
2023-06-10 12:16:38 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -12,18 +12,20 @@ import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
 import chatbotRoutes from "./routes/Chatbot.js";
 import otpRoutes from './routes/Otp.js'
-const app = express();
-app.use(express.json({limit: "30mb", extended: true}))
-app.use(express.urlencoded({limit: "30mb", extended: true}))
-app.use(
-    cors({
-        origin:"http://localhost:3000",
-    })
-);
+
+
 
 dotenv.config()
 connectDB()
-const PORTS = process.env.PORT
+
+const PORTS = process.env.PORT || 5000
+
+
+const app = express();
+app.use(express.json({limit: "30mb", extended: true}))
+app.use(express.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
+
 
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
